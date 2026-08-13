6cba5993259f0110bae157ad9794a387cdad0755
= <=>
2026-08-12 21:21:09 -0500

    se adiciona logica de zoom para la imagen del producto y estilos css, tambien se cambia app.tsx para evitar la centralizacion del contenido

--- motor-libre/src/components/product/productImage.tsx
+++ motor-libre/src/components/product/productImage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { Product } from "../../types/detailedProduct";
 
 interface ProductImageProps {
@@ -8,13 +9,49 @@ export function ProductImage({
   product,
 }:  ProductImageProps) {
 
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0
+  });
+
+    const [showZoom, setShowZoom] = useState(false);
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+
+    const { left, top, width, height } =
+      event.currentTarget.getBoundingClientRect();
+
+    const x = ((event.clientX - left) / width) * 100;
+    const y = ((event.clientY - top) / height) * 100;
+
+    setPosition({ x, y });
+  };
+
   return (
-    <div>
+    <div
+      className="product-image-container"
+      onMouseMove={handleMouseMove}
+      onMouseEnter={() => setShowZoom(true)}
+      onMouseLeave={() => setShowZoom(false)} 
+    >
       <img
         src={product.image}
         alt={product.name}
-        className="img-fluid"
+        className="img-fluid product-image"
       />
+
+      {showZoom && (
+        <div
+          className="image-zoom"
+          style={{
+            left: `${position.x}%`,
+            top: `${position.y}%`,
+            backgroundImage: `url(${product.image})`,
+            backgroundPosition: `${position.x}% ${position.y}%`
+          }}
+        />
+      )}
+
     </div>
   );
 }
