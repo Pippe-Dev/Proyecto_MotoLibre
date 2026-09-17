4ab444aec83320ee0a8c2efeb21abf4257283580
= <=>
2026-09-16 19:12:57 -0500

    se aplican estilos para visualizacion de productos

--- motor-libre/src/components/product/productImage.tsx
+++ motor-libre/src/components/product/productImage.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import type { Product } from "../../types/detailedProduct";
+import "./styles/productImageStyles.css";
 
 interface ProductImageProps {
   product: Product;
@@ -7,16 +8,22 @@ interface ProductImageProps {
 
 export function ProductImage({
   product,
-}:  ProductImageProps) {
+}: ProductImageProps) {
+
+  const [selectedImage, setSelectedImage] = useState(0);
 
   const [position, setPosition] = useState({
     x: 0,
-    y: 0
+    y: 0,
   });
 
-    const [showZoom, setShowZoom] = useState(false);
+  const [showZoom, setShowZoom] = useState(false);
 
-  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+  const currentImage = product.images[selectedImage];
+
+  const handleMouseMove = (
+    event: React.MouseEvent<HTMLDivElement>
+  ) => {
 
     const { left, top, width, height } =
       event.currentTarget.getBoundingClientRect();
@@ -27,29 +34,84 @@ export function ProductImage({
     setPosition({ x, y });
   };
 
-  return (
-    <div
-      className="product-image-container"
-      onMouseMove={handleMouseMove}
-      onMouseEnter={() => setShowZoom(true)}
-      onMouseLeave={() => setShowZoom(false)} 
-    >
-      <img
-        src={product.image}
-        alt={product.name}
-        className="img-fluid product-image"
-      />
+  const nextImage = () => {
+    setSelectedImage((prev) =>
+      prev === product.images.length - 1
+        ? 0
+        : prev + 1
+    );
+  };
 
-      {showZoom && (
-        <div
-          className="image-zoom"
-          style={{
-            left: `${position.x}%`,
-            top: `${position.y}%`,
-            backgroundImage: `url(${product.image})`,
-            backgroundPosition: `${position.x}% ${position.y}%`
-          }}
-        />
+  const previousImage = () => {
+    setSelectedImage((prev) =>
+      prev === 0
+        ? product.images.length - 1
+        : prev - 1
+    );
+  };
+
+  return (
+    <div className="product-gallery">
+
+      <div
+        className="product-image-container"
+        onMouseMove={handleMouseMove}
+        onMouseEnter={() => setShowZoom(true)}
+        onMouseLeave={() => setShowZoom(false)}
+      >
+
+        {product.images.length > 1 && (
+          <>
+            <button
+              className="gallery-arrow left"
+              onClick={previousImage}
+            >
+              ❮
+            </button>
+
+            <button
+              className="gallery-arrow right"
+              onClick={nextImage}
+            >
+              ❯
+            </button>
+          </>
+        )}
+
+         <img src={currentImage} alt={product.name} />
+
+        {showZoom && (
+          <div
+            className="image-zoom"
+            style={{
+              left: `${position.x}%`,
+              top: `${position.y}%`,
+              backgroundImage: `url(${currentImage})`,
+              backgroundPosition: `${position.x}% ${position.y}%`,
+            }}
+          />
+        )}
+      </div>
+
+      {product.images.length > 1 && (
+        <div className="thumbnail-container">
+
+          {product.images.map((image, index) => (
+            <button
+              key={image}
+              type="button"
+              className={`thumbnail ${
+                selectedImage === index
+                  ? "active"
+                  : ""
+              }`}
+              onClick={() => setSelectedImage(index)}
+            >
+              <img className="thumbnail-image" src={image} alt={product.name} />
+            </button>
+          ))}
+
+        </div>
       )}
 
     </div>
--- motor-libre/src/components/product/productInfo.tsx
+++ motor-libre/src/components/product/productInfo.tsx
@@ -51,13 +51,19 @@ export function ProductInfo({product,}: ProductInfoProps) {
         {product.name}
       </h1>
 
-      <h2 className="price-product">
+      <h1 className="price-product">
         ${product.price.toLocaleString("es-CO")}
-      </h2>
+      </h1>
 
       <p className="title-product">
         {product.description}
       </p>
+
+      <p style={{ fontWeight: "bold" , color: "green"}}>
+        Stock disponible: {product.stock} unidades
+      </p>
+
+      <p style={{ fontWeight: "bold" , color: "black"}}>Cantidad: </p>
 
       <div className="d-flex align-items-center gap-2 mt-4">
         <QuantitySelector
@@ -65,13 +71,14 @@ export function ProductInfo({product,}: ProductInfoProps) {
           quantity={quantity}
           onQuantityChange={setQuantity}
         />
+      </div>
 
-        <button className="btn btn-primary btn-add-cart"
+      <br />
+      <button className="btn btn-primary btn-add-cart bi bi-cart-fill mt-3"
         onClick={handleAddToCart}
         >
           AÑADIR AL CARRITO
         </button>
-      </div>
 
 
     </div>
